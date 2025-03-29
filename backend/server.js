24e81fc919119e9abc48a4fadd275766ae2d4a39
Franklin Marcelin <55675901+franklinm54@users.noreply.github.com>
2025-03-29 00:28:14 -0400

    Initial setup: Project structure and basic configuration
    - Setup basic Express.js project structure
    - Add package.json with dev and start scripts
    - Create backend directory structure (routes, controllers, lib)
    - Setup MongoDB connection configuration
    - Add .gitignore file

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// routes
+import authRoutes from "./routes/auth.route.js";
+
+// db
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () =>{
+    console.log(`Server running on http://localhost:${PORT}`);
+    
+    // connect to db
+    connectDB();
+});
+
