df4c1ec0e54741be96919d6b6ba876c665e853d1
Franklin Marcelin <55675901+franklinm54@users.noreply.github.com>
2025-04-08 21:43:54 -0400

    feat: Implement user signup functionality
    - Added logic to the `signup` controller to handle user registration.
    - Implemented a check to prevent duplicate user creation based on email.
    - Returns a 400 status if the user already exists.
    - Returns a 201 status upon successful user creation.
    - Included basic error handling with a try/catch block.

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // (Middleware) allows you to parse JSON request bodies
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () =>{
